ddb3f47e5a5e3d66a74e23be37dc6233c76f9687
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-09-07 14:53:39 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// api_key=4568c047ac29c58c34b8c5ba81189d61
+
+// const options = {
+//   key: '4568c047ac29c58c34b8c5ba81189d61',
+// };
+
+// fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${options.key}`)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//   });
+
+// //   https://developers.themoviedb.org/3/
+// // https://api.themoviedb.org/3/movie/157336
