ccde69705decbcc47cf27334dbc1a1b2dab7d755
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-09-08 11:18:39 +0300

    Commit-TabVers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 // api_key=4568c047ac29c58c34b8c5ba81189d61
+function reverseStr(arr1, arr2) {
+  let total = [];
+  arr1.forEach(item => {
+    if (arr2.join(' ').includes(item)) {
+      total.push(item);
+      return total;
+    }
+    return [];
+  });
 
-// const options = {
-//   key: '4568c047ac29c58c34b8c5ba81189d61',
-// };
+  return total;
+}
 
-// fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${options.key}`)
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data);
-//   });
+const a1 = ['arp', 'live', 'strong'];
+const a2 = ['lively', 'alive', 'harp', 'sharp', 'armstrong'];
+const b1 = ['tarp', 'mice', 'bull'];
+const b2 = ['lively', 'alive', 'harp', 'sharp', 'armstrong'];
+// returns[('arp', 'live', 'strong')];
 
-// //   https://developers.themoviedb.org/3/
-// // https://api.themoviedb.org/3/movie/157336
+console.log(reverseStr(a1, a2));
+console.log(reverseStr(b1, b2));
