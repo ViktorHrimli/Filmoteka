77a943603f3f282511abc96b6a332480bea30c64
ViktorHrimli <95180031+ViktorHrimli@users.noreply.github.com>
2022-09-08 08:19:16 +0000

    Deploying to gh-pages from @ ViktorHrimli/ViktorHrimli@ccde69705decbcc47cf27334dbc1a1b2dab7d755 🚀

---
+++ index.b6037904.js
@@ -0,0 +1,2 @@
+function l(l,r){let o=[];return l.forEach((l=>r.join(" ").includes(l)?(o.push(l),o):[])),o}console.log(l(["arp","live","strong"],["lively","alive","harp","sharp","armstrong"])),console.log(l(["tarp","mice","bull"],["lively","alive","harp","sharp","armstrong"]));
+//# sourceMappingURL=index.b6037904.js.map
---
+++ index.ea3699a0.js
@@ -0,0 +1,2 @@
+!function(){function r(r,n){var l=[];return r.forEach((function(r){return n.join(" ").includes(r)?(l.push(r),l):[]})),l}console.log(r(["arp","live","strong"],["lively","alive","harp","sharp","armstrong"])),console.log(r(["tarp","mice","bull"],["lively","alive","harp","sharp","armstrong"]))}();
+//# sourceMappingURL=index.ea3699a0.js.map
